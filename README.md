cebb387d35abace568bb86da27d55dafa415a15d
Sean P. Kane <spkane00@gmail.com>
2025-06-06 15:00:04 -0700

    Update README and code files for MacroPad project

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # pimoroni-trackball-circuitpy
-An adequate CircuitPython-compatible library for using the Pimoroni Trackball module as a mouse. Based on [Pimoroni's official Python library](https://github.com/pimoroni/trackball-python), which is incompatible with CircuitPython.
 
-I don't have a totally clear understanding of how the Pimoroni Trackball works under the hood or why their official library does some of the things that it does. Anyway, I find the Trackball unsuitable for desktop OS use as it loses precision at higher acceleration/sensitivity value (set by the `multiplier` in the code.py file). Maybe someone smarter than me can fix this, but I suspect it's a hardware issue (e.g. the ball is too small to have the precision of a traditional trackball mouse).
+A fork of a basic [CircuitPython-compatible library for using the Pimoroni Trackball module as a mouse](https://github.com/jspinella/pimoroni-trackball-circuitpy). 
+
+I forked this, becuase in my project the trackball is mounted in such a way, that I needed to modify the directions to get the pointer to do what I expected. It is possible this is a bug in the original code, or even a change in the device or libraries, but `"It works for me."`
+
+## Upstream README
+
+Based on [Pimoroni's official Python library](https://github.com/pimoroni/trackball-python), which is incompatible with CircuitPython.
+
+I (_jspinella_) don't have a totally clear understanding of how the Pimoroni Trackball works under the hood or why their official library does some of the things that it does. Anyway, I find the Trackball unsuitable for desktop OS use as it loses precision at higher acceleration/sensitivity value (set by the `multiplier` in the code.py file). Maybe someone smarter than me can fix this, but I suspect it's a hardware issue (e.g. the ball is too small to have the precision of a traditional trackball mouse).
 
 ## Dependencies
-This code is tested to work with CircuitPython 7 and two of the libraries from the [Adafruit CircuitPython 7 bundle](https://circuitpython.org/libraries):
+This code is tested to work with CircuitPython 7-10 and two of the libraries from the [Adafruit CircuitPython bundle](https://circuitpython.org/libraries):
 
 - adafruit_bus_device
 - adafruit_hid
--- code.py
+++ code.py
@@ -1,10 +1,14 @@
-import usb_hid
 import board
 import busio
+import usb_hid
 from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_hid.mouse import Mouse
 
 I2C_ADDRESS = 0x0A
+I2C_ADDRESS_ALTERNATIVE = 0x0B
+
+CHIP_ID = 0xBA11
+VERSION = 1
 
 REG_LED_RED = 0x00
 REG_LED_GRN = 0x01
@@ -15,23 +19,53 @@ REG_LEFT = 0x04
 REG_RIGHT = 0x05
 REG_UP = 0x06
 REG_DOWN = 0x07
+REG_SWITCH = 0x08
+MSK_SWITCH_STATE = 0b10000000
+
+REG_USER_FLASH = 0xD0
+REG_FLASH_PAGE = 0xF0
+REG_INT = 0xF9
+MSK_INT_TRIGGERED = 0b00000001
+MSK_INT_OUT_EN = 0b00000010
+REG_CHIP_ID_L = 0xFA
+RED_CHIP_ID_H = 0xFB
+REG_VERSION = 0xFC
+REG_I2C_ADDR = 0xFD
+REG_CTRL = 0xFE
+MSK_CTRL_SLEEP = 0b00000001
+MSK_CTRL_RESET = 0b00000010
+MSK_CTRL_FREAD = 0b00000100
+MSK_CTRL_FWRITE = 0b00001000
 
 # i2c @ address 0xA
 m = Mouse(usb_hid.devices)
 i2c = busio.I2C(board.SCL, board.SDA)
 device = I2CDevice(i2c, I2C_ADDRESS)
 
-# intensity of r,g,b,w LEDs from 0-100 e.g. set_leds(100,100,25,50)
-def set_leds(r,g,b,w):
-    device.write(bytes([REG_LED_RED, r & 0xff]))
-    device.write(bytes([REG_LED_GRN, g & 0xff]))
-    device.write(bytes([REG_LED_BLU, b & 0xff]))
-    device.write(bytes([REG_LED_WHT, w & 0xff]))
 
-def set_leds_purple(): set_leds(60,0,90,20)
-def set_leds_orange(): set_leds(99,63,8,0)
-def set_leds_yellow(): set_leds(100,85,6,0)
-def set_leds_white(): set_leds(0,0,0,100)
+# intensity of r,g,b,w LEDs from 0-100 e.g. set_leds(100,100,25,50)
+def set_leds(r, g, b, w):
+    device.write(bytes([REG_LED_RED, r & 0xFF]))
+    device.write(bytes([REG_LED_GRN, g & 0xFF]))
+    device.write(bytes([REG_LED_BLU, b & 0xFF]))
+    device.write(bytes([REG_LED_WHT, w & 0xFF]))
+
+
+def set_leds_purple():
+    set_leds(60, 0, 90, 20)
+
+
+def set_leds_orange():
+    set_leds(99, 63, 8, 0)
+
+
+def set_leds_yellow():
+    set_leds(100, 85, 6, 0)
+
+
+def set_leds_white():
+    set_leds(0, 0, 0, 100)
+
 
 def i2c_rdwr(data, length=0):
     """Write and optionally read I2C data."""
@@ -44,22 +78,24 @@ def i2c_rdwr(data, length=0):
 
     return []
 
+
 def read():
-    """Read up, down, left, right and switch data from trackball."""
-    left, right, up, down, switch = i2c_rdwr([REG_LEFT], 5)
+    """Read down, up, left, right and switch data from trackball."""
+    left, right, down, up, switch = i2c_rdwr([REG_LEFT], 5)
 
     switch = 129 == switch
 
-    return up, down, left, right, switch
+    return left, right, down, up, switch
+
 
 with device:
-    #set_leds(0,0,5,100)
+    # set_leds(0,0,5,100)
     set_leds_purple()
 
     multiplier = 9
 
     while True:
-        up, down, left, right, switch = read()
+        down, up, left, right, switch = read()
 
         # Send movements and clicks to xte
         if switch:
@@ -67,4 +103,4 @@ with device:
         else:
             x = right - left
             y = down - up
-            m.move(int(-x*multiplier), int(-y*multiplier), 0)
+            m.move(int(-x * multiplier), int(-y * multiplier), 0)
